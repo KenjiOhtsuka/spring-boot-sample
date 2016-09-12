ec45aa2f511de023eef024513bab0e33a6442065
Kenji Ohtsuka <kok.fdcm@gmail.com>
2016-09-12 17:25:29 +0900

    initial commit

---
+++ src/main/java/com/example/myproject/Application.kt
@@ -0,0 +1,17 @@
+package com.example.myproject
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+open class Application {
+    companion object {
+        @JvmStatic fun main(args: Array<String>) {
+            SpringApplication.run(Application::class.java, *args)
+        }
+    }
+}
